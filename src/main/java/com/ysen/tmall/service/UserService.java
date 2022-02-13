bace8d64b551491a403569e5935cfe13e6a44d65
DESKTOP-QQ6NGGP\ysens <1607392378@qq.com>
2022-02-13 10:56:29 +0800

    后台-用户管理

---
+++ src/main/java/com/ysen/tmall/service/UserService.java
@@ -0,0 +1,18 @@
+package com.ysen.tmall.service;
+
+import com.ysen.tmall.entity.User;
+
+import java.util.List;
+
+public interface UserService {
+
+    void add(User c);
+
+    void delete(int id);
+
+    void update(User c);
+
+    User get(int id);
+
+    List list();
+}
